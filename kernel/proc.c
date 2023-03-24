67895730a72dd27d171be9e95d64ed0fddeec42d
mehrdad mahabadi <mehrdad.mahabadi@snapp.cab>
2023-03-24 20:17:12 +0330

    feat: add nprocs to proc.c

--- kernel/proc.c
+++ kernel/proc.c
@@ -714,3 +714,21 @@ found:
   release(&tickslock);
   return xticks - ticks0;
 }
+
+
+// Returns number of currently in use processes
+int 
+nprocs(void) { 
+
+  struct proc *p; 
+  int n = 0; 
+
+  for(p = proc; p < &proc[NPROC]; p++){
+    acquire(&p->lock); 
+    if (p->state != UNUSED) 
+      n++; 
+    release(&p->lock); 
+  }
+
+  return n ; 
+}
