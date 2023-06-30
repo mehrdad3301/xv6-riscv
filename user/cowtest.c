8337ee96b18e49d346185bc369bcabdf33c8db83
mehrdad mahabadi <mehrdad.mahabadi@snapp.cab>
2023-06-30 12:43:19 +0330

    feat: add simple test

--- user/cowtest.c
+++ user/cowtest.c
@@ -3,16 +3,46 @@
 #include "kernel/sysinfo.h"
 #include "user/user.h"
 
+void
+simpletest()
+{
+  uint64 phys_size = 128*1024*1024 ; 
+  int sz = (phys_size / 3) * 2;
+
+  printf("simple: ");
+  
+  char *p = sbrk(sz);
+  if(p == (char*)0xffffffffffffffffL){
+    printf("sbrk(%d) failed\n", sz);
+    exit(-1);
+  }
+
+  for(char *q = p; q < p + sz; q += 4096){
+    *(int*)q = getpid();
+  }
+
+  int pid = fork();
+  if(pid < 0){
+    printf("fork() failed\n");
+    exit(-1);
+  }
+
+  if(pid == 0)
+    exit(0);
+
+  wait(0);
+
+  if(sbrk(-sz) == (char*)0xffffffffffffffffL){
+    printf("sbrk(-%d) failed\n", sz);
+    exit(-1);
+  }
+
+  printf("ok\n");
+}
 int 
 main(int argc, char *argv[])
-{ 
-    struct sysinfo sinfo ;
-    if (sysinfo(&sinfo) < 0) { 
-        fprintf(2, "sysinfo: error in system call"); 
-        exit(-1); 
-    }
+{
 
-    sbrk(sinfo.totalram * 2 / 3) ;
-    fork() ;
-    exit(0); 
+    simpletest()  ; 
+    exit(0)  ; 
 }
